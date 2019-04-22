f57f0ab3b6debe8479870c0b293a7675e9d6149d
shravan1998 <shrkum51@gmail.com>
2019-04-22 11:38:30 +0530

    Having problem with sprites

---
+++ Assets/Sunnyland/artwork/Sprites/player/run/move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public float speed;
+    public float acceleartion;
+
+    private float currentspeed;
+    private float targetspeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetspeed = Input.GetAxisRow
+    }
+}
