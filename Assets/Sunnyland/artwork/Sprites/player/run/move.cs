9502dd95e798b153cfd7954181840f9ba8126331
shravan1998 <shrkum51@gmail.com>
2019-05-22 23:27:21 +0530

    Jump worked

--- Assets/Sunnyland/artwork/Sprites/player/run/move.cs
+++ Assets/Sunnyland/artwork/Sprites/player/run/move.cs
@@ -5,17 +5,27 @@ using System;
 
 public class move : MonoBehaviour
 {
+    public LayerMask groundLayers;
+    public Rigidbody rb;
     public float speed=0.01F;
     public float jumpspeed=1F;
+    bool ground = false;
+    //public transform groundCheck;
+    float groundRadius=0.2f;
+    private bool shouldJump;
+    private bool canJump;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+      //  grounded = Physics2D.OverlapCircle(groundCheck.position,groundRadius,groundLayers);
+        //anim.setBool("Grounded",grounded);
+
         if(Input.GetKey(KeyCode.D))
         {
             transform.Translate(new Vector3(speed,0,0));
@@ -26,11 +36,13 @@ public class move : MonoBehaviour
         }
         if(Input.GetKey(KeyCode.W))
         {
-            transform.Translate(new Vector3(0,jumpspeed,0));
+           rb.AddForce(new Vector2(0,0.5F));
         }
-        if(Input.GetKey(KeyCode.X)){
-            transform.Translate(new Vector3(0,-jumpspeed,0));
+        if(Input.GetKey(KeyCode.S)){
+           rb.AddForce(new Vector2(0,-0.5F));
+           
         }
+       
     }
    
     
